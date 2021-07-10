092f54909406810ed47ac23911b805052e1e0960
Anna Kowalska <a.kowalska622@gmail.com>
2021-07-10 09:59:01 +0200

    Cart logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,28 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context';
 import CartItem from './CartItem';
 
 const Cart = props => {
+  const [reachedMax, setReachedMax] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const hasItems = cartCtx.items.length > 0;
 
-  const removeCartItem = id => {};
+  const removeCartItem = id => {
+    cartCtx.removeItem(id);
+  };
 
-  const addCartItem = item => {};
+  const addCartItem = item => {
+    const itemIdx = cartCtx.items.findIndex(x => x.name === item.name);
+    if (cartCtx.items[itemIdx].amount >= 5) {
+      setReachedMax(true);
+      return;
+    }
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -30,6 +41,7 @@ const Cart = props => {
 
   return (
     <Modal onCartClose={props.onCartClose}>
+      {reachedMax && <small>You can buy maximum 5 plants of each kind</small>}
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
